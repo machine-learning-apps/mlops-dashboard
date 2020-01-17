21ac7e8bae53f95d346bcd2738c755a1e8f8f1c4
Hamel Husain <hamel.husain@gmail.com>
2020-01-16 20:36:11 -0800

    fix link?

--- docs/_site/assets/js/just-the-docs.js
+++ docs/_site/assets/js/just-the-docs.js
@@ -44,7 +44,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'http://localhost:4000/mlops-dashboard/assets/js/search-data.json', true);
+  request.open('GET', 'http://localhost:4000/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
